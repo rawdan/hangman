a80ca5328f3bc7fadcaa111e43e0a2ed1baf8669
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-07 20:42:30 +0300

    pass new word from dictionary module to hangman module, split characters and output them

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -1,6 +1,13 @@
 defmodule Hangman.Game do
 
+  defstruct(
+    turn_left: 7,
+    game_state: :initializing,
+    letters:    [],
+  )
   def new_game() do
-
+    %Hangman.Game{
+      letters: Dictionary.random_word |> String.codepoints
+    }
   end
 end
