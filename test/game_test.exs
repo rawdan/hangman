c2b34a62307716de32232bfe16df431e8c1742f6
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-07 21:06:33 +0300

    spec: add test for state change

--- test/game_test.exs
+++ test/game_test.exs
@@ -7,5 +7,14 @@ defmodule GameTest do
 
     assert game.turn_left  == 7
     assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "state isn't changed for :won game " do
+    game = Game.new_game()
+    game = Map.put(game, :game_state, :won)
+    { new_game, _ } = Game.make_move(game, "x")
+
+    assert new_game == game
   end
 end
