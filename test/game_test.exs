4b5190757a228b6bb72126fd1bd32e72e3362ac7
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-07 21:14:27 +0300

    refactor: remove duplication in test

--- test/game_test.exs
+++ test/game_test.exs
@@ -10,15 +10,10 @@ defmodule GameTest do
     assert length(game.letters) > 0
   end
 
-  test "state isn't changed for :won game " do
-    game = Game.new_game() |> Map.put(:game_state, :won)
-    assert { ^game, _ } = Game.make_move(game, "x")
-
-  end
-
-  test "state isn't changed for :lost game " do
-    game = Game.new_game() |> Map.put(:game_state, :lost)
-    assert { ^game, _ } = Game.make_move(game, "x")
-
+  test "state isn't changed for :won or :lost games " do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
   end
 end
