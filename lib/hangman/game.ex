984a66907ba3d3418f22b05166a2b4e39951262e
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-08 10:34:16 +0300

    refactor: segregate public and private interface

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -26,49 +26,53 @@ defmodule Hangman.Game do
     { game, tally(game) }
   end
 
-  def accept_move(game, guess, _already_guessed = true) do
+  def tally(game) do
+    %{
+      game_state: game.game_state,
+      turns_left: game.turns_left,
+      letters:    game.letters |> reveal_guessed(game.used)
+    }
+  end
+
+
+#############################################################
+
+
+  defp accept_move(game, guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
 
-  def accept_move(game, guess, _already_guessed) do
+  defp accept_move(game, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
-  def score_guess(game, _good_guess = true) do
+  defp score_guess(game, _good_guess = true) do
     new_state = MapSet.new(game.letters)
     |> MapSet.subset?(game.used)
     |> maybe_won
     Map.put(game, :game_state, new_state)
   end
 
-  def score_guess(game = %{ turns_left: 1}, _not_good_gues) do
+  defp score_guess(game = %{ turns_left: 1}, _not_good_gues) do
     Map.put(game, :game_state, :lost)
   end
 
-  def score_guess(game = %{ turns_left: turns_left}, _not_good_gues) do
+  defp score_guess(game = %{ turns_left: turns_left}, _not_good_gues) do
     %{ game |
     game_state: :bad_guess,
     turns_left: turns_left - 1
      }
   end
 
-  def tally(game) do
-    %{
-    game_state: game.game_state,
-    turns_left: game.turns_left,
-    letters:    game.letters |> reveal_guessed(game.used)
-    }
-  end
-
-  def reveal_guessed(letters, used) do
+  defp reveal_guessed(letters, used) do
     letters
     |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter)) end)
   end
 
-  def reveal_letter(letter, _in_word = true), do: letter
-  def reveal_letter(letter, _not_in_word),    do: "_"
+  defp reveal_letter(letter, _in_word = true), do: letter
+  defp reveal_letter(letter, _not_in_word),    do: "_"
 
-  def maybe_won(true), do: :won
-  def maybe_won(_),    do: :good_guess
+  defp maybe_won(true), do: :won
+  defp maybe_won(_),    do: :good_guess
 end
